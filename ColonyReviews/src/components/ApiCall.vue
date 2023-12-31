85fe3260d7cbc94527a28ca2fa340dfa2789996d
Yohan Garcia <yoanvaldes03@gmail.com>
2023-12-30 19:50:40 -0500

    first commit

---
+++ ColonyReviews/src/components/ApiCall.vue
@@ -0,0 +1,23 @@
+new Vue({
+  el: '#GetBlogs',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://localhost:7011/BlogPost')
+        .then((response) => {
+          // Display the response in the console
+          console.log(response);
+
+          // Set the response data to the component's data property
+          this.info = response.data;
+        })
+        .catch((error) => {
+          // Handle errors
+          console.error('Error fetching data:', error);
+        });
+  }
+})
